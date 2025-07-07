e96d7980007a0709cdf994e148eb624565f56903
kalpit_project <kalpitkarna49@gmail.com>
2025-07-07 22:15:36 +0530

    'added about me and clickable button'

---
+++ script.js
@@ -0,0 +1,14 @@
+// Add Navbar to all page
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetch ('navbar.html')
+    .then(response => response.text())
+    .then (data => { 
+        document.getElementById('navbar-placeholder').innerHTML = data;
+    })
+    .catch (error => {
+        console.log('Error Loading Navbar:', error); 
+    });
+});
+
+// 
