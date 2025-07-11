0da6520a400b0de5234149341a34c57b664f6e04
kalpit_project <kalpitkarna49@gmail.com>
2025-07-11 22:22:52 +0530

    'added more content to home page and new about page'

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 // Add Navbar to all page
 
-document.addEventListener('DOMContentLoaded', function() {
-    fetch ('navbar.html')
-    .then(response => response.text())
-    .then (data => { 
-        document.getElementById('navbar-placeholder').innerHTML = data;
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("navbar.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("navbar-placeholder").innerHTML = data;
     })
-    .catch (error => {
-        console.log('Error Loading Navbar:', error); 
+    .catch((error) => {
+      console.log("Error Loading Navbar:", error);
     });
 });
-
-// Add 
